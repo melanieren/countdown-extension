1ac19223316564aaad761178f89cc9b0310bd1f5
Molan Ren <ren.molan@gmail.com>
2017-08-10 11:42:09 -0400

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,87 @@
+var myForm = document.getElementById("newEventForm");
+var createEventBtn = document.getElementById("createEvent");
+var submitBtn = document.getElementById("add");
+var hideBtn = document.getElementById("hide");
+var clearBtn = document.getElementById("clear");
+
+createEventBtn.onclick = function showForm() {
+  myForm.style.display = 'block';
+  submitBtn.style.display = 'inline';
+  hideBtn.style.display = 'inline';
+  clearBtn.style.display = 'inline';
+  createEventBtn.style.display = 'none';
+}
+
+hideBtn.onclick = function hideForm() {
+  myForm.style.display = 'none';
+  submitBtn.style.display = 'none';
+  hideBtn.style.display = 'none';
+  clearBtn.style.display = 'none';
+  createEventBtn.style.display = 'block';
+}
+
+clearBtn.onclick = function clearForm() {
+  myForm.reset();
+}
+
+document.getElementById("add").onclick = function() {
+  var eventLine = document.createElement("p");
+  var timerLine = document.createElement("p");
+  var text = document.getElementById("evName").value;
+
+  if (text === "") { 
+    document.getElementById("err").innerHTML = "Please enter a valid event name!";  
+  } else { 
+
+    // Create the countdown timer
+
+    var hourInDay = 24;
+    var minInHour = 60;
+    var secInMin = 60;
+    var millisecInSec = 1000;
+
+    var countDownDate = document.getElementById("evDate").value;
+    var countDownTime = Date.parse(countDownDate);
+    var offset = new Date().getTimezoneOffset();
+    var eventTime = document.getElementById("evTime").value;
+    var timeSplit = eventTime.split(":");
+    var timeMillisec = (timeSplit[0] * minInHour * secInMin * millisecInSec) + (timeSplit[1] * secInMin * millisecInSec);
+
+    var timer = setInterval(function() {
+      var now = new Date().getTime(); 
+      var distance = countDownTime - now + (offset * secInMin * millisecInSec) + timeMillisec; 
+
+      if (distance < 0) {
+        clearInterval(timer);
+        eventLine.innerHTML = "<li>" + text + ":" + "</li>";
+        timerLine.innerHTML = "Made it!";
+      } else if (isNaN(distance)) {
+        clearInterval(timer);
+        document.getElementById("err").innerHTML = "Please enter a valid date and time!";
+      } else {
+        eventLine.innerHTML = "<li>" + text + ":" + "</li>";
+        var days = Math.floor(distance / (hourInDay * minInHour * secInMin * millisecInSec));
+        var hours = Math.floor((distance % (hourInDay * minInHour * secInMin * millisecInSec)) / (minInHour * secInMin * millisecInSec));
+        var minutes = Math.floor((distance % (minInHour * secInMin * millisecInSec)) / (secInMin * millisecInSec));
+        var seconds = Math.floor((distance % (secInMin * millisecInSec)) / millisecInSec);
+        timerLine.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      }
+    }, 1000);
+
+    // Add new event to list
+
+    document.getElementById("eventList").appendChild(eventLine);
+    document.getElementById("eventList").appendChild(timerLine);
+    document.getElementById("err").innerHTML = "";  
+    document.getElementById("emptyList").innerHTML = "";  
+
+    // Hide form
+
+    myForm.style.display = 'none';
+    submitBtn.style.display = 'none';
+    hideBtn.style.display = 'none';
+    clearBtn.style.display = 'none';
+    createEventBtn.style.display = 'block';
+    myForm.reset();
+  }
+};
